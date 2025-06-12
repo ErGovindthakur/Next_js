671262ceea2a8dce5f19540fc94383ba26047edf
ErGovindthakur <ergovindthakur@gmail.com>
2025-06-12 16:48:57 +0530

    set up code snippet project with sadcn ui and prisma orm with sqlite file based db

---
+++ code_snippets/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button className='bg-blue-500 text-white'>New</Button>
+    </div>
+  )
+}
+
+export default page
