bb791efe80e01c19c322b87e3dde794b3ee59f99
ErGovindthakur <ergovindthakur@gmail.com>
2025-07-01 00:15:09 +0530

    add create todo feature

--- Full_Stack_Todo_Auth/server/src/Todo/TodoController.ts
+++ Full_Stack_Todo_Auth/server/src/Todo/TodoController.ts
@@ -1,2 +1,53 @@
 // Writing code to perform crud over the todo
 
+import { Request, Response, NextFunction } from "express";
+import TodoModel from "./TodoModel";
+
+export const createTodo = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { todoTitle, todoTask } = req.body;
+  if (!todoTask || !todoTitle) {
+    return res.status(400).json({
+      success: false,
+      message: "All fields are required",
+    });
+  }
+
+  try {
+    const data = await TodoModel.find();
+
+    await TodoModel.create({
+      todoTitle,
+      todoTask,
+    });
+
+    res.status(201).json({
+      success: true,
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error,
+    });
+  }
+};
+
+export const updateTodo = async(req: Request, res: Response, next: NextFunction) => {
+     const {id} = req.params;
+     const {todoTitle,todoTask} = req.body;
+
+  try {
+     const todo = await TodoModel.findByIdAndUpdate(id,)
+
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error,
+    });
+  }
+};
--- Full_Stack_Todo_Auth/server/src/app.ts
+++ Full_Stack_Todo_Auth/server/src/app.ts
@@ -1,6 +1,13 @@
 import express from "express"
+import todoRouter from "./Todo/TodoRoutes";
+import bodyParser from "body-parser"
 
 const app = express();
 
+// Middleware
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use("/api/todo",todoRouter)
 
 export default app
